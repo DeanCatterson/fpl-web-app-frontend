fc61efe17ecd0275c0d37042af4ad63c3a391a7f
Dean Catterson <deancatterson32@gmail.com>
2024-09-03 22:19:55 +0100

    Connect TeamIdField to backend

--- src/team/TeamIdField.js
+++ src/team/TeamIdField.js
@@ -1,24 +1,40 @@
+import axios from "axios";
+import React, { useState } from "react";
+
 import Form from 'react-bootstrap/Form';
 
 function TeamIdField() {
-    const submitForm = (event) => {
+    const [team, setTeam] = useState({});
+    const [teamId, setTeamId] = useState(0);
+
+    const handleTeamIdChange = (event) => {
+        setTeamId(event.target.value);
+      };
+
+
+    const submitForm = async (event) => {
+        setTeamId(event.target.value);
+
         event.preventDefault();
+        console.log('YYYYY event: ', event)
+        console.log('YYYYY event.target.value: ', event.target.value)
 
-        const formData = new FormData(event.target);
-        const payload = Object.fromEntries(formData);
+        let response = await axios.get(`http://localhost:8080/team/${teamId}`);
 
-        console.log('XXXXX payload: ', payload);
+        let team = response.data;
+        setTeam(team);
+
+        console.log('XXXXX team: ', team);
     }
 
     return (
         <div>
-
             <h1>Enter your team id</h1>
 
             <form onSubmit={submitForm}>
                 <Form.Group className='mb3'>
                     <Form.Label>Team ID</Form.Label>
-                    <Form.Control type='text' name='teamID' placeholder='Enter your Team ID' />
+                    <Form.Control type='text' name='teamID' onChange={handleTeamIdChange} placeholder='Enter your Team ID' />
                 </Form.Group>
             </form>
         </div>
