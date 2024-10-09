e3a814984c8b983c5450603cd10ae84b00bef771
Dean Catterson <deancatterson32@gmail.com>
2024-10-09 21:46:16 +0100

    Test layout of team history under TeamDetails

--- src/team/TeamHistory.js
+++ src/team/TeamHistory.js
@@ -10,8 +10,28 @@ function TeamHistory({ history }) {
 
     return (
         <div className='teamHistoryContainer'>
-            <a>Hello World!</a>
-            <p> No of completed seasons: {history.numberOfCompletedSeasons} </p>
+            <h2>Current Season</h2>
+            <p> Highest Overall Rank: {history.currentSeasonHighestOverallRank} </p>
+            <p> Highest Weekly Rank: {history.currentSeasonHighestWeeklyRank} </p>
+            <p> Highest Weekly Points: {history.currentSeasonHighestWeeklyPoints} </p>
+
+            <br />
+
+            {
+                (history.numberOfCompletedSeasons && history.numberOfCompletedSeasons > 0)
+                    ?
+                        <div>
+                            <h2>All time</h2>
+                            <p> Best yearly finish: {history.highestYearlyFinishRank} in {history.highestYearlyFinishRankSeason}</p>
+                            <p> Best yearly points: {history.highestYearlyFinishPoints} in {history.highestYearlyFinishPointsSeason}</p>
+                            <p> Average yearly rank: {history.averageYearlyRank} </p>
+                            <p> Average yearly points: {history.averageYearlyPoints} </p>
+                            <p> No of completed seasons: {history.numberOfCompletedSeasons} </p>
+                        </div>
+                    :
+                        null
+            }
+
         </div>
     );
 }
